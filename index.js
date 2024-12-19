fa809ab07f422005f1e7643793f66875d1f05f90
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-12-19 14:17:06 +0530

    FRONTEND ADDED TO THE APPLICATION

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require("mongoose");
+const path = require('path');
 
 const { urlRouter } = require("./routes/urlRoutes");
 
@@ -11,6 +12,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use("" , urlRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URL);
